ca039904ef480a81dbbdd43096907d950200bd38
Francisco Casillas <chiccasillas@gmail.com>
2025-07-14 10:30:51 -0600

    Add model classes: Dish and Restaurant

--- app/src/main/java/com/franciscocasillas/cdmxgourmet/models/Dish.java
+++ app/src/main/java/com/franciscocasillas/cdmxgourmet/models/Dish.java
@@ -1,4 +1,15 @@
 package com.franciscocasillas.cdmxgourmet.models;
 
 public class Dish {
+    public String name;
+    public double price;
+    public String description;
+    public String imageUrl;
+
+    public Dish(String name, double price, String description, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
 }
